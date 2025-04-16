19a492752156058cba24037ae1cacaa46e0420f5
Thomas Waldmann <tw@waldmann-edv.de>
2025-04-16 14:28:41 +0200

    It's 2025.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'XStatic'
-copyright = '2011-2024, Thomas Waldmann'
+copyright = '2011-2025, Thomas Waldmann'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
